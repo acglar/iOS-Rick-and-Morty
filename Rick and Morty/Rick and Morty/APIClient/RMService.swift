eb4c8c66961bd2286442e66d3e8e1cf1089b9b8a
Caglar <asdwas94@hotmail.com>
2023-05-24 22:38:48 +0300

    Created API call

--- Rick and Morty/Rick and Morty/APIClient/RMService.swift
+++ Rick and Morty/Rick and Morty/APIClient/RMService.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 final class RMService {
-    let instance = RMService()
+    static let instance = RMService()
     
     private init() { }
     
@@ -18,6 +18,32 @@ final class RMService {
     ///   - type: The of the object we expect to return
     ///   - onComplete: Callback with data or error
     public func execute<T: Codable>(_ request: RMRequest, expecting type: T.Type, onComplete: @escaping (Result<T, Error>) -> Void) {
+        guard let urlRequest = self.request(from: request) else {
+            onComplete(.failure(RMServiceError.failedToCreateRequest))
+            return
+        }
         
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, _, error in
+            guard let data = data, error == nil else {
+                onComplete(.failure(error ?? RMServiceError.failedToGetData))
+                return
+            }
+            
+            // Decode response
+            do {
+                let result = try JSONDecoder().decode(type.self, from: data)
+                onComplete(.success(result))
+            } catch {
+                onComplete(.failure(error))
+            }
+        }
+        task.resume()
+    }
+    
+    private func request(from rmRequest: RMRequest) -> URLRequest? {
+        guard let url = rmRequest.url else { return nil }
+        var request = URLRequest(url: url)
+        request.httpMethod = rmRequest.httpMethod
+        return request
     }
 }
