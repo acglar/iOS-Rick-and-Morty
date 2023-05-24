bbbea9562bbecc161a21e7279fea8f090d900929
Caglar <asdwas94@hotmail.com>
2023-05-24 22:39:03 +0300

    Added Error types for api call

---
+++ Rick and Morty/Rick and Morty/APIClient/RMServiceError.swift
@@ -0,0 +1,13 @@
+//
+//  RMServiceError.swift
+//  Rick and Morty
+//
+//  Created by Ali ÇAĞLAR on 24.05.2023.
+//
+
+import Foundation
+
+enum RMServiceError: Error {
+    case failedToCreateRequest
+    case failedToGetData
+}
