4074f65164ce8435f247169d05d158637a92cfeb
Caglar <asdwas94@hotmail.com>
2023-05-25 00:02:08 +0300

    Instantiated character list view

--- Rick and Morty/Rick and Morty/Controllers/Core/RMCharacterViewController.swift
+++ Rick and Morty/Rick and Morty/Controllers/Core/RMCharacterViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 final class RMCharacterViewController: UIViewController {
+    
+    private let characterListView = CharacterListView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Characters"
         view.backgroundColor = .systemBackground
+        view.addSubview(characterListView)
+        
+        NSLayoutConstraint.activate([
+            characterListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            characterListView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            characterListView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            characterListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
 
 }
