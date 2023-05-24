f352a30d7662e343a1363e9a0bfe585dd47e5547
Caglar <asdwas94@hotmail.com>
2023-05-25 00:00:44 +0300

    Added Extension for views to add multiple subviews at once

---
+++ Rick and Morty/Rick and Morty/Resources/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  Rick and Morty
+//
+//  Created by Ali ÇAĞLAR on 24.05.2023.
+//
+
+import UIKit
+
+extension UIView {
+    func addSubviews(_ views: UIView...) {
+        views.forEach({
+            addSubview($0)
+        })
+    }
+}
