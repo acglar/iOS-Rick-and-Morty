ec9b32e0efda0ee7b708460460a2b711ef546e48
Caglar <asdwas94@hotmail.com>
2023-06-15 19:57:29 +0300

    Removed search button on the settings tab

--- Rick and Morty/Rick and Morty/Controllers/Core/RMSettingsViewController.swift
+++ Rick and Morty/Rick and Morty/Controllers/Core/RMSettingsViewController.swift
@@ -18,7 +18,6 @@ final class RMSettingsViewController: UIViewController {
         
         title = "Settings"
         view.backgroundColor = .systemBackground
-        addSearchBar()
         addSwiftUIController()
     }
     
@@ -50,10 +49,6 @@ final class RMSettingsViewController: UIViewController {
         self.settingsSwiftUIController = settingsSwiftUIController
     }
     
-    private func addSearchBar() {
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(didTapSearchBar))
-    }
-    
     private func handleTap(option: RMSettingsOption) {
         guard Thread.current.isMainThread else { return }
         
@@ -67,11 +62,5 @@ final class RMSettingsViewController: UIViewController {
             let viewController = SFSafariViewController(url: url)
             present(viewController, animated: true)
         }
-    }
-    
-    @objc
-    private func didTapSearchBar() {
-        let viewController = RMSearchViewController(config: .init(type: .character))
-        navigationController?.pushViewController(viewController, animated: true)
     }
 }
